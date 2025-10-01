65b7607d491d2fe9760fc60178067d5782378ec6
AndrewTucker18 <a.j.tucker@wustl.edu>
2025-10-01 15:42:51 -0500

    Flag Done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,24 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(.166,.496,.166,.166);
+		StdDraw.filledRectangle(.496+.332,.166,.166,.166);
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(.496,.332,.166,.332);
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(.166,.166,.166,.166);
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(.496,.332,.166,.332);
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(.496+.332,.496,.166,.166);
+		for (int i = 1; i<4; i++){
+			for (int j = 0; j<3; j++){
+				StdDraw.filledCircle(0.083*i, (.583 - 0.083*j), 0.03);
+			}
+		}
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(0.5,0.332,0.5,0.332);
+
 	}
 }
