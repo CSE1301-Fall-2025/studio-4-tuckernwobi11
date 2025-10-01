991c9e506941850d5773b181a6115c6cc82d73f6
snwobi23 <snwobi@brooksschool.org>
2025-10-01 15:10:54 -0500

    commit for switch

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -7,7 +7,9 @@ import java.util.Scanner;
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
+
 import edu.princeton.cs.introcs.StdDraw;
+import java.awt.Color;
 
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
@@ -19,6 +21,36 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		Color color = new Color(in.nextInt(),in.nextInt(),in.nextInt());
+		StdDraw.setPenColor(color);
+		boolean fill = in.nextBoolean();
+		if(shape.equals("rectangle")){
+			System.out.println(shape);
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWitdh = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			if(fill == true) {
+				StdDraw.filledRectangle(x,y,halfWitdh,halfHeight);
+			} else {
+				StdDraw.rectangle(x,y,halfWitdh,halfHeight);
+			}
+		} else if (shape.equals("ellipse")) {
+			System.out.println(shape);
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double semiMajor = in.nextDouble();
+			double semiMinor = in.nextDouble();
+			if(fill == true) {
+				StdDraw.filledEllipse(x,y,semiMajor,semiMinor);
+			} else {
+				StdDraw.ellipse(x,y,semiMajor,semiMinor);
+				System.out.println(semiMajor);
+				System.out.print(semiMinor);
+			}
+		}
 		
+
 	}
 }
